68881c1e401395e17b5877b437b97e273e27ed7c
Kamil <kamil.segebre@new10.com>
2022-05-18 19:14:52 +0200

    chore: proper spdx-license-identifier
    Based on this website: https://spdx.org/licenses/

--- force-app/main/default/lwc/domainProcessBindingListItem/domainProcessBindingListItem.js
+++ force-app/main/default/lwc/domainProcessBindingListItem/domainProcessBindingListItem.js
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2022, Kamil Segebre
  * All rights reserved.
- * SPDX-License-Identifier: BSD 3-Clause
+ * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { api, LightningElement } from 'lwc'
