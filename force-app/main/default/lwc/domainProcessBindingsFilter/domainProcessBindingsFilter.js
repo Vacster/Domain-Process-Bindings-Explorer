7928751778115b84bb35c91a3dd374c60215c4fc
Kamil <kamil.segebre@new10.com>
2022-05-18 19:10:39 +0200

    chore: add BSD 3-Clause LICENSE file and also to code
    If any other license if preferred I'm willing to change this for another one (e.g. MIT)

--- force-app/main/default/lwc/domainProcessBindingsFilter/domainProcessBindingsFilter.js
+++ force-app/main/default/lwc/domainProcessBindingsFilter/domainProcessBindingsFilter.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022, Kamil Segebre
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD 3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 import getEntityDefinitions from '@salesforce/apex/DomainBindingExplorerController.getEntityDefinitions';
 import { LightningElement, wire } from 'lwc';
 
--- force-app/main/default/lwc/entityDefinitionSelector/entityDefinitionSelector.js
+++ force-app/main/default/lwc/entityDefinitionSelector/entityDefinitionSelector.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022, Kamil Segebre
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD 3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 import { api, LightningElement, wire } from 'lwc';
 import getEntityDefinitions from '@salesforce/apex/DomainBindingExplorerController.getEntityDefinitions';
 
