7fca0f0278adac1186bd1fe892edd5adba227a7d
Kamil <kamil.segebre@new10.com>
2022-05-20 15:56:17 +0200

    refactor: removed unnecessary elements from filter

--- force-app/main/lwc/domainProcessBindingsFilter/domainProcessBindingsFilter.js
+++ force-app/main/lwc/domainProcessBindingsFilter/domainProcessBindingsFilter.js
@@ -4,33 +4,21 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import getEntityDefinitions from '@salesforce/apex/DomainBindingExplorerController.getEntityDefinitions'
-import { LightningElement, wire } from 'lwc'
+import { LightningElement } from 'lwc'
 
-const POSSIBLE_ACTIONS = [
+export const POSSIBLE_ACTIONS = [
     { label: 'Created', value: 'create' },
     { label: 'Updated', value: 'update' },
     { label: 'Deleted', value: 'delete' },
 ]
 
 export default class DomainProcessBindingsFilter extends LightningElement {
-    _selectedSObjectDeveloperName = ''
     _selectedAction = POSSIBLE_ACTIONS[0]
 
-    @wire(getEntityDefinitions)
-    entityDefinitionsWire(value) {
-        if (value.data) {
-            if (!this._selectedSObjectDeveloperName) {
-                this._selectedSObjectDeveloperName = value.data[0].DeveloperName
-            }
-        }
-    }
-
     handleObjectChanged(event) {
-        this._selectedSObjectDeveloperName = event.detail
         this.dispatchEvent(
             new CustomEvent('objectchanged', {
-                detail: this._selectedSObjectDeveloperName,
+                detail: event.detail,
             })
         )
     }
@@ -48,10 +36,6 @@ export default class DomainProcessBindingsFilter extends LightningElement {
 
     get possibleActions() {
         return POSSIBLE_ACTIONS
-    }
-
-    get selectedSObjectDeveloperName() {
-        return this._selectedSObjectDeveloperName
     }
 
     get selectedActionLabel() {
